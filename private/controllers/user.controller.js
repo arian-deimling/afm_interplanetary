9aa1bf3ba95d7bcd819d1509b82629db94161edc
Arian Deimling <arianm@deimling.net>
2022-04-04 13:24:57 -0400

    cleaned up user controller code

--- private/controllers/user.controller.js
+++ private/controllers/user.controller.js
@@ -1,27 +1,30 @@
-
-
-const bcrypt = require('bcrypt');
-const session = require("express-session");
 const util = require('util');
+const bcrypt = require('bcrypt');
 
-const { saltRounds } = require('../config/hash.config');
-const { usernameRegex, passwordRegex, securityQuestionAnswerRegex } = require('../../public/static/config/validation.config.js');
 const db = require('../models');
-const res = require('express/lib/response');
+const { saltRounds } = require('../config/hash.config');
+const { usernameRegex, passwordRegex, securityQuestionAnswerRegex } = 
+  require('../../public/static/config/validation.config.js');
 
 const User = db.user;
 const SecurityQuestion = db.security_question;
 
-const login = async (req, res) => {
+// promisify the bcrypt.compare() and bcrypt.hash() methods
+const bcryptCompare = util.promisify(bcrypt.compare);
+const bcryptHash = util.promisify(bcrypt.hash);
 
-  if (!req.body.username) {
+// controller for login requests from the client
+const login = async (req, res) => {
+  // make sure that the request contains username and password
+  const clientCredentials = req.body;
+  if (!clientCredentials.username) {
     res.status(400).send({
       what: 'username',
       message: 'Username is required.',
     });
     return;
   }
-  if (!req.body.password) {
+  if (!clientCredentials.password) {
     res.status(400).send({
       what: 'password',
       message: 'Password is required.',
@@ -29,24 +32,20 @@ const login = async (req, res) => {
     return;
   }
 
-  const userLogin = {
-    username: req.body.username,
-    password: req.body.password,
-  };
-
-  let users;
-
   // get users with matching username from the users table in the db
+  let usersQueryResult;
   try {
-    users = await User.findAll({ where: { username: userLogin.username, }, });
-
+    usersQueryResult = await User.findAll({
+      where: { username: clientCredentials.username },
+    });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
     return;
   }
 
-  if (users.length === 0) {
+  // if no users match respond with an error message
+  if (usersQueryResult.length === 0) {
     res.status(400).send({
       what: 'username',
       message: 'User does not exist.',
@@ -54,49 +53,51 @@ const login = async (req, res) => {
     return;
   }
 
-  let passwordCompare;
-
+  // compare client login password with stored password for this user
+  const storedPassword = usersQueryResult[0].dataValues.password;
+  let passwordCompareResult;
   try {
-    passwordCompare = await new Promise((resolve, reject) => {
-      bcrypt.compare(userLogin.password, users[0].dataValues.password, (err, result) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      });
-    });
-
+    passwordCompareResult = await bcryptCompare(
+      clientCredentials.password, storedPassword
+    );
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
     return;
   }
 
-  if (passwordCompare === true) {
-    req.session.username = userLogin.username;
-    res.sendStatus(200);
-    return;
-  } else {
+  // if password does not match respond with an error message
+  if (passwordCompareResult !== true) {
     res.status(400).send({
       what: 'password',
       message: 'Password is incorrect.',
     });
     return;
   }
+
+  // if login is successful, set session username and send OK status
+  req.session.username = clientCredentials.username;
+  res.sendStatus(200);
+  return;
 };
 
+// controller for account creation requests from the client
 const create = async (req, res) => {
+  // save min and max values for security question ID
+  const SECURITY_QUESTION_ID_MIN = 1;
+  const SECURITY_QUESTION_ID_MAX = await SecurityQuestion.max('id');
 
-  SECURITY_QUESTION_ID_MIN = 1;
-  SECURITY_QUESTION_ID_MAX = await SecurityQuestion.max('id');
+  const clientCredentials = req.body;
 
-  if (!req.body.username) {
+  // validate username from client
+  if (!clientCredentials.username) {
     res.status(400).send({
       what: 'username',
       message: 'Username is required.',
     });
     return;
-  } else if (!usernameRegex.test(req.body.username)) {
+  }
+  if (!usernameRegex.test(clientCredentials.username)) {
     res.status(400).send({
       what: 'username',
       message: 'Username is invalid.',
@@ -104,13 +105,15 @@ const create = async (req, res) => {
     return;
   }
 
-  if (!req.body.password) {
+  // validate password from client
+  if (!clientCredentials.password) {
     res.status(400).send({
       what: 'password',
       message: 'Password is required.',
     });
     return;
-  } else if (!passwordRegex.test(req.body.password)) {
+  }
+  if (!passwordRegex.test(clientCredentials.password)) {
     res.status(400).send({
       what: 'password',
       message: 'Password is invalid.',
@@ -118,13 +121,15 @@ const create = async (req, res) => {
     return;
   }
 
-  if (!req.body.pass_verify) {
+  // validate password verification field from client
+  if (!clientCredentials.pass_verify) {
     res.status(400).send({
       what: 'pass_verify',
       message: 'Password Confirmation is required.',
     });
     return;
-  } else if (req.body.pass_verify !== req.body.password) {
+  }
+  if (clientCredentials.pass_verify !== clientCredentials.password) {
     res.status(400).send({
       what: 'pass_verify',
       message: 'Password Confirmation is invalid.',
@@ -132,13 +137,19 @@ const create = async (req, res) => {
     return;
   }
   
-  if (!req.body.security_question_id) {
+  // validate security question ID from client
+  if (!clientCredentials.security_question_id) {
     res.status(400).send({
       what: 'security_question_id',
       message: 'Security Question is required.',
     });
     return;
-  } else if (!(req.body.security_question_id >= SECURITY_QUESTION_ID_MIN  && req.body.security_question_id <= SECURITY_QUESTION_ID_MAX)) {
+  }
+  const validSecurityQuestionId = (
+    (clientCredentials.security_question_id >= SECURITY_QUESTION_ID_MIN)
+    || (clientCredentials.security_question_id <= SECURITY_QUESTION_ID_MAX)
+  );
+  if (!validSecurityQuestionId) {
     res.status(400).send({
       what: 'security_question_id',
       message: 'Security Question is invalid.',
@@ -146,13 +157,15 @@ const create = async (req, res) => {
     return;
   }
 
-  if (!req.body.security_question_answer) {
+  // validate security question answer from client
+  if (!clientCredentials.security_question_answer) {
     res.status(400).send({
       what: 'security_question_answer',
       message: 'Security Question Answer is required.',
     });
     return;
-  } else if (!securityQuestionAnswerRegex.test(req.body.username)) {
+  }
+  if (!securityQuestionAnswerRegex.test(clientCredentials.security_question_answer)) {
     res.status(400).send({
       what: 'security_question_answer',
       message: 'Security Question Answer is invalid.',
@@ -160,70 +173,59 @@ const create = async (req, res) => {
     return;
   }
 
+  // get users with matching username from the users table in the db
+  let usersQueryResult;
+  try {
+    usersQueryResult = await User.findAll({
+      where: { username: clientCredentials.username },
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
+
+  // if an entry with the same username is found in the db, respond to client
+  // with error message
+  if (usersQueryResult.length !== 0) {
+    res.status(400).send({
+      what: 'username',
+      message: 'Username is already taken, please try a different one.',
+    });
+    return;
+  }
+  
   const newUser = {
-    username: req.body.username,
-    password: req.body.password,
-    security_question_id: req.body.security_question_id,
-    security_question_answer: req.body.security_question_answer,
+    username: clientCredentials.username,
+    security_question_id: clientCredentials.security_question_id,
   };
 
-  /**
-   * Steps for registering a user:
-   *  1. salt the password provided by the user
-   *  2. salt the security question answer provided by the user
-   *  3. add the new user to the db
-   */
-  bcrypt.hash(newUser.password, saltRounds, (err, hash) => {
-    // if an error occurs while hashing the password send an error message
-    // to the client
-    if (err) {
-      console.log(err.message);
-      res.sendStatus(500);
-      return;
-    }
-    // update the user password to the hashed password value
-    newUser.password = hash;
+  // hash the client values for password and security question answer
+  try {
+    newUser.password = await bcryptHash(clientCredentials.password, saltRounds);
+    newUser.security_question_answer = await bcryptHash(
+      clientCredentials.security_question_answer, saltRounds
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
 
-    bcrypt.hash(newUser.security_question_answer, saltRounds, (err, hash) => {
-      // if an error occurs while hashing the security question answer send an
-      // error message to the client
-      if (err) {
-        console.log(err.message);
-        res.sendStatus(500);
-        return;
-      }
-      // update the user security question to the hashed value
-      newUser.security_question_answer = hash;
+  // create a new user with the client input values
+  try {
+    await User.create(newUser);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
 
-      // attempt to create the new user in the database
-      User.create(newUser)
-
-      // if the user is create successfully
-      .then(data => {
-        res.sendStatus(200);
-      })
-
-      // if an error occurs creating the user
-      .catch(err => {
-
-        // if the username already exists, send an appropriate error message
-        if (err.name === 'SequelizeUniqueConstraintError') {
-          res.status(400).send({
-            what: 'username',
-            message: 'Username is already taken, please try a different one.',
-          });
-          return;
-        }
-
-        // if an unknown error occurs, log the error message
-        console.log(err.message)
-        res.sendStatus(500);
-      });
-    });
-  });
+  // respond to the client with OK
+  res.sendStatus(200);
 };
 
-
+// controller to check whether a user is logged in
 const checkLogin = (req, res) => {
   // send OK status if there is a valid, active session; otherwise 400
   if (req.session.username) {
@@ -233,6 +235,7 @@ const checkLogin = (req, res) => {
   }
 };
 
+// controller to log a user out
 const logout = (req, res) => {
   if (req.session.username) {
     req.session.username = '';
@@ -242,9 +245,13 @@ const logout = (req, res) => {
   }
 };
 
+// controller to find user security question to assist with password reset
 const findUser = async (req, res) => {
 
-  if (!req.body.username) {
+  clientCredentials = req.body;
+
+  // validate username from client
+  if (!clientCredentials.username) {
     res.status(400).send({
       what: 'username',
       message: 'Username is required.',
@@ -252,23 +259,21 @@ const findUser = async (req, res) => {
     return;
   }
 
-  const userLogin = {
-    username: req.body.username,
-  };
-
-  let users;
 
   // get users with matching username from the users table in the db
+  let usersQueryResult;
   try {
-    users = await User.findAll({ where: { username: userLogin.username, }, });
-
+    usersQueryResult = await User.findAll({
+      where: { username: clientCredentials.username }, 
+    });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
     return;
   }
 
-  if (users.length === 0) {
+  // if the user does not exist, respond with an error message
+  if (usersQueryResult.length === 0) {
     res.status(400).send({
       what: 'username',
       message: 'User does not exist.',
@@ -276,54 +281,67 @@ const findUser = async (req, res) => {
     return;
   }
 
-  // TODO(AD) - add try/catch
-  const question = await SecurityQuestion.findAll({ where: { id: users[0].dataValues.security_question_id } });
+  // find the security question for the user
+  let questionQueryResult;
+  try {
+    questionQueryResult = await SecurityQuestion.findAll({
+      where: { id: usersQueryResult[0].dataValues.security_question_id },
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
 
+  // respond the the client with the user's security question
   res.status(200).send({
-    security_question: question[0].dataValues.question,
-    security_question_id: users[0].dataValues.security_question_id,
+    security_question: questionQueryResult[0].dataValues.question,
+    security_question_id: usersQueryResult[0].dataValues.security_question_id,
   });
 };
 
+// controller to reset the user's password
 const resetPassword = async (req, res) => {
 
-  if (!req.body.username) {
+  const clientCredentials = req.body;
+
+  // validate input from the client
+  if (!clientCredentials.username) {
     res.status(400).send({
       what: 'username',
       message: 'Username is required.',
     });
     return;
   }
-
-  if (!req.body.security_question_answer) {
+  if (!clientCredentials.security_question_answer) {
     res.status(400).send({
       what: 'security_question_answer',
       message: 'Security Question Answer is required.',
     });
     return;
   }
-
-  if (!req.body.password) {
+  if (!clientCredentials.password) {
     res.status(400).send({
       what: 'password',
       message: 'Password is required.',
     });
     return;
-  } else if (!passwordRegex.test(req.body.password)) {
+  }
+  if (!passwordRegex.test(clientCredentials.password)) {
     res.status(400).send({
       what: 'password',
       message: 'Password is invalid.',
     });
     return;
   }
-
-  if (!req.body.pass_verify) {
+  if (!clientCredentials.pass_verify) {
     res.status(400).send({
       what: 'pass_verify',
       message: 'Password Confirmation is required.',
     });
     return;
-  } else if (req.body.pass_verify !== req.body.password) {
+  }
+  if (clientCredentials.pass_verify !== clientCredentials.password) {
     res.status(400).send({
       what: 'pass_verify',
       message: 'Password Confirmation is invalid.',
@@ -332,17 +350,19 @@ const resetPassword = async (req, res) => {
   }
 
   // get users with matching username from the users table in the db
-  let users;
+  let usersQueryResult;
   try {
-    users = await User.findAll({ where: { username: req.body.username, }, });
+    usersQueryResult = await User.findAll({
+      where: { username: clientCredentials.username },
+    });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(500);
     return;
   }
 
-  // if user does not exist return an error message
-  if (users.length === 0) {
+  // if user does not exist respond with an error message
+  if (usersQueryResult.length === 0) {
     res.status(400).send({
       what: 'username',
       message: 'User does not exist.',
@@ -351,43 +371,13 @@ const resetPassword = async (req, res) => {
   }
 
   // compare security question answer to value stored in db
-  const bcryptCompare = util.promisify(bcrypt.compare);
-  const securityQuestionAnswer = req.body.security_question_answer;
-  const securityQuestionAnswerHash = users[0].dataValues.security_question_answer;
-  let securityQuestionAnswerCompare;
+  const securityQuestionAnswer = clientCredentials.security_question_answer;
+  const securityQuestionAnswerHash = 
+    usersQueryResult[0].dataValues.security_question_answer;
+  let securityQuestionAnswerCompareResult;
   try {
-    securityQuestionAnswerCompare = await bcryptCompare(securityQuestionAnswer, securityQuestionAnswerHash);
-  } catch (err) {
-    console.log(err.message);
-    res.sendStatus(500);
-    return;
-  }
-
-  // if security question answer was incorrect, send a message to the client
-  if (!securityQuestionAnswerCompare) {
-    res.status(400).send({
-      what: 'security_question_answer',
-      message: 'Security question answer is incorrect.',
-    });
-  }
-
-  // hash the users new password
-  const bcryptHash = util.promisify(bcrypt.hash);
-  const newPassword = req.body.password;
-  let newPasswordHash;
-  try {
-    newPasswordHash = await bcryptHash(newPassword, saltRounds);
-  } catch (err) {
-    console.log(err.message);
-    res.sendStatus(500);
-    return;
-  }
-
-  // update the user password
-  try {
-    await User.update(
-      { password: newPasswordHash },
-      { where: { username: req.body.username } }
+    securityQuestionAnswerCompareResult = await bcryptCompare(
+      clientCredentials.security_question_answer, securityQuestionAnswerHash
     );
   } catch (err) {
     console.log(err.message);
@@ -395,6 +385,37 @@ const resetPassword = async (req, res) => {
     return;
   }
 
+  // if security question answer was incorrect, send a message to the client
+  if (!securityQuestionAnswerCompareResult) {
+    res.status(400).send({
+      what: 'security_question_answer',
+      message: 'Security question answer is incorrect.',
+    });
+  }
+
+  // hash the user's new password
+  let newPasswordHash;
+  try {
+    newPasswordHash = await bcryptHash(clientCredentials.password, saltRounds);
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
+
+  // update the user's password
+  try {
+    await User.update(
+      { password: newPasswordHash },
+      { where: { username: clientCredentials.username } },
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(500);
+    return;
+  }
+
+  // respond to the client with OK status code
   res.sendStatus(200);
 };
 
